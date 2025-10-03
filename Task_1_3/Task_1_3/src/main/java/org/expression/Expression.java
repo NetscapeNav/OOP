e54a8fa5f497d72b1c53553bcb727cf14fb39517
Stepan Kuchenkov <s.kuchenkov@g.nsu.ru>
2025-10-03 21:28:28 +0700

    Delete double folder

--- Task_1_3/Task_1_3/src/main/java/org/expression/Expression.java
+++
@@ -1,9 +0,0 @@
-package org.expression;
-
-public abstract class Expression {
-    public abstract int method();
-    public abstract void print();
-    public abstract Expression derivative(String varName);
-    public abstract int eval(String equation);
-}
-
