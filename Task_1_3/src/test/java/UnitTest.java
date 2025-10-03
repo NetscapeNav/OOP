ff0d5ca7a97ed26b6b1ecf5e95e1cb8714e1f61a
Stepan Kuchenkov <s.kuchenkov@g.nsu.ru>
2025-10-03 22:22:55 +0700

    Adding a test

---
+++ Task_1_3/src/test/java/UnitTest.java
@@ -0,0 +1,22 @@
+import org.expression.*;
+import org.expression.Number;
+import org.expression.Variable;
+import org.expression.Add;
+import org.expression.Sub;
+import org.expression.Mul;
+import org.expression.Div;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UnitTest {
+    @Test
+    void testNumber() {
+        Number num = new Number(5);
+        assertEquals(5, num.method());
+
+        Expression de = num.derivative("x");
+        assertEquals(0, de);
+        assertEquals(5, num.eval("x = 10"));
+    }
+}
