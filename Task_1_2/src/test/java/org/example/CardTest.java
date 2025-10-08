2d54411bc281802b3455b1697abef166330bcd3d
Stepan Kuchenkov <s.kuchenkov@g.nsu.ru>
2025-10-08 19:42:28 +0700

    Add localized test

--- Task_1_2/src/test/java/org/example/CardTest.java
+++ Task_1_2/src/test/java/org/example/CardTest.java
@@ -1,19 +1,27 @@
 package org.example;
 
+import java.util.Locale;
 import game.model.Card;
 import game.model.Rank;
 import game.model.Suit;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import ui.LocalizationManager;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CardTest {
+    @BeforeAll
+    static void setup() {
+        LocalizationManager.loadBundle(new Locale("ru"));
+    }
+
     @Test
     void testToString() {
         Card queenOfHearts = new Card(Rank.QUEEN, Suit.HEARTS);
-        assertEquals("Queen of Hearts", queenOfHearts.toString());
+        assertEquals("Дама Червей", queenOfHearts.toString());
         Card tenOfSpades = new Card(Rank.TEN, Suit.SPADES);
-        assertEquals("10 of Spades", tenOfSpades.toString());
+        assertEquals("10 Пик", tenOfSpades.toString());
     }
 
     @Test
--- Task_1_2/src/test/java/org/example/ConsoleInputTest.java
+++ Task_1_2/src/test/java/org/example/ConsoleInputTest.java
@@ -1,16 +1,24 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ui.ConsoleInput;
+import ui.LocalizationManager;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Locale;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ConsoleInputTest {
+    @BeforeAll
+    static void setup() {
+        LocalizationManager.loadBundle(new Locale("ru"));
+    }
+
     @Test
     void testInputInvalidInput() {
         PrintStream originalOut = System.out;
@@ -18,7 +26,7 @@ public class ConsoleInputTest {
         System.setOut(new PrintStream(outContent));
         try (ConsoleInput testInput = new ConsoleInput(new Scanner("abc\n1"))) {
             int decision = testInput.getPlayerDecision();
-            assertTrue(outContent.toString().contains("Invalid input!"));
+            assertTrue(outContent.toString().contains("Неверный ввод! Введите 0 или 1."));
             assertEquals(1, decision);
         } finally {
             System.setOut(originalOut);
