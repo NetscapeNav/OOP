6557511d3e6840619a28bafb0a81a7258a9837a3
Stepan Kuchenkov <s.kuchenkov@g.nsu.ru>
2025-11-06 02:38:03 +0700

    adding a test to look that it works

--- Task_2_2/src/test/java/HashTableTest.java
+++ Task_2_2/src/test/java/HashTableTest.java
@@ -1,12 +1,33 @@
 import hashtable.HashTable;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HashTableTest {
+    private HashTable<String, Integer> table;
+
+    @BeforeEach
+    void setUp() {
+        table = new HashTable<>();
+    }
+
     @Test
-    void test() {
+    void testPutAndGet() {
+        table.put("key1", 100);
+        assertEquals(100, table.get("key1"));
+    }
+
+    @Test
+    void testPutOverwrite() {
+        table.put("key1", 100);
+        table.put("key1", 200);
+        assertEquals(200, table.get("key1"));
+    }
+
+    @Test
+    void testExamplePutUpdateGet() {
         HashTable<String, Number> hashTable = new HashTable<>();
         hashTable.put("one", 1);
         hashTable.update("one", 1.0);
