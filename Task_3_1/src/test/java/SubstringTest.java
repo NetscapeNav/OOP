48745ebae6bd95cd8dd93a087d03eb547ff7b50b
Stepan Kuchenkov <s.kuchenkov@g.nsu.ru>
2025-11-19 15:30:55 +0700

    expanding test

--- Task_3_1/src/test/java/SubstringTest.java
+++ Task_3_1/src/test/java/SubstringTest.java
@@ -2,6 +2,7 @@ import file.SubstringSearch;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,12 +11,20 @@ public class SubstringTest {
     @Test
     public void test() throws IOException {
         try {
-            SubstringSearch finder = new SubstringSearch();
             List<Long> results = SubstringSearch.find("src/test/resources/test.txt", "бра");
             List<Long> expected = List.of(1L, 8L);
             assertEquals(expected, results);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Test
+    public void testNull() throws IOException {
+        List<Long> resultsNull = SubstringSearch.find("src/test/resources/test.txt", null);
+        assertEquals(Collections.emptyList(), resultsNull, "Null pattern should return empty list.");
+
+        List<Long> resultsEmpty = SubstringSearch.find("src/test/resources/test.txt", "");
+        assertEquals(Collections.emptyList(), resultsEmpty, "Empty pattern should return empty list.");
     }
 }
