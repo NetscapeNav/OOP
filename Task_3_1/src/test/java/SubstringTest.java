e3614feb5d48ff78378b3d1d69752c1ba136fe77
Stepan Kuchenkov <s.kuchenkov@g.nsu.ru>
2025-11-19 15:21:53 +0700

    adding test

--- Task_3_1/src/test/java/SubstringTest.java
+++ Task_3_1/src/test/java/SubstringTest.java
@@ -1,2 +1,21 @@
+import file.SubstringSearch;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SubstringTest {
+    @Test
+    public void test() {
+        try {
+            SubstringSearch finder = new SubstringSearch();
+            List<Long> results = SubstringSearch.find("test.txt", "бра");
+            List<Long> expected = List.of(1L, 8L);
+            assertEquals(expected, results);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
