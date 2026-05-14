a6e33d5099f0d37c2a722382635b60089cfe5a09
Stepan Kuchenkov <s.kuchenkov@g.nsu.ru>
2026-05-15 01:55:44 +0700

    Changing whole architecture of project

---
+++ Task_2_1_2/src/main/java/prime_first/DistributedProtocol.java
@@ -0,0 +1,18 @@
+package prime_first;
+
+final class DistributedProtocol {
+    static final int DEFAULT_CHUNK_SIZE = 1024;
+
+    static final int CONNECT_TIMEOUT_MS = 1000;
+    static final int HEARTBEAT_INTERVAL_MS = 500;
+    static final int HEARTBEAT_TIMEOUT_MS = 3000;
+    static final int RETRY_DELAY_MS = 300;
+
+    static final int MSG_ACCEPTED = 1;
+    static final int MSG_HEARTBEAT = 2;
+    static final int MSG_RESULT = 3;
+    static final int MSG_ERROR = 4;
+
+    private DistributedProtocol() {
+    }
+}
