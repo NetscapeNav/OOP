40f549405701b732f78fc3b8ed44c66dd6919761
NetscapeNav <NetscapeNav@users.noreply.github.com>
2026-05-19 04:52:17 +0000

    Deploying to gh-pages from @ NetscapeNav/OOP@b9420714338ab64c2da00934603b3e2f699f90c3 🚀

--- Task_2_1_2/package-search-index.js
+++ Task_2_1_2/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.example"},{"l":"prime_first"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"master"},{"l":"org.example"},{"l":"prime_common"},{"l":"worker"}];updateSearchResults();
