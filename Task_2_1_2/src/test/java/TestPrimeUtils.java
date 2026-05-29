21d49b455643a1f8d56614c8aa75ae6433377b26
Stepan Kuchenkov <s.kuchenkov@g.nsu.ru>
2026-05-29 14:42:20 +0700

    Add tests to Primeutils

--- Task_2_1_2/src/test/java/TestPrimeUtils.java
+++ Task_2_1_2/src/test/java/TestPrimeUtils.java
@@ -2,6 +2,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.example.common.PrimeUtils;
 
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class TestPrimeUtils {
     @Test
     public void testPrimeNumbers() {
@@ -15,5 +18,19 @@ public class TestPrimeUtils {
         Assertions.assertTrue(PrimeUtils.isComposite(4));
         Assertions.assertTrue(PrimeUtils.isComposite(9));
         Assertions.assertTrue(PrimeUtils.isComposite(2147483646));
+    }
+
+    @Test
+    public void testCancellationBeforeCalculation() {
+        Assertions.assertThrows(CancellationException.class,
+                () -> PrimeUtils.isComposite(2147483647, () -> true));
+    }
+
+    @Test
+    public void testCancellationDuringCalculation() {
+        AtomicInteger checks = new AtomicInteger(0);
+
+        Assertions.assertThrows(CancellationException.class,
+                () -> PrimeUtils.isComposite(2147483647, () -> checks.incrementAndGet() > 1));
     }
 }
